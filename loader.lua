89fe98e0eea44847be4ef87eee1633b0d1b6a09e
Dev <79853619+devvedbydev@users.noreply.github.com>
2024-10-10 00:37:31 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -13,7 +13,7 @@ end
 local scripts = {
     { url = "https://raw.githubusercontent.com/devvedbydev/Systemhook/refs/heads/main/aimlock.lua", name = "Aim Lock" },
     { url = "https://raw.githubusercontent.com/devvedbydev/Systemhook/refs/heads/main/cursor.lua", name = "Cursor" },
-    { url = "https://raw.githubusercontent.com/devvedbydev/Systemhook/refs/heads/main/skeletonesp.lua", name = "Skeleton ESP" },
+    { url = "https://raw.githubusercontent.com/devvedbydev/Systemhook/refs/heads/main/esp.lua", name = "Skeleton ESP" },
     { url = "https://raw.githubusercontent.com/devvedbydev/Systemhook/refs/heads/main/overlay.lua", name = "Overlay" },
 }
 
