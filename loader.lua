c28b10729fe6d6b2fef6c522585f61e710d27180
Dev <79853619+devvedbydev@users.noreply.github.com>
2024-10-09 23:11:42 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,4 @@
-loadstring(HttpGet("https://raw.githubusercontent.com/hitbyharibo/byfron/main/aimlock.lua"))()
-loadstring(HttpGet("https://raw.githubusercontent.com/hitbyharibo/byfron/main/cursor.lua"))()
-loadstring(HttpGet("https://raw.githubusercontent.com/hitbyharibo/byfron/main/skeletonesp.lua"))()
-loadstring(HttpGet("https://raw.githubusercontent.com/hitbyharibo/byfron/main/overlay.lua"))()
+loadstring(HttpGet("https://raw.githubusercontent.com/devvedbydev/Systemhook/refs/heads/main/aimlock.lua"))()
+loadstring(HttpGet("https://raw.githubusercontent.com/devvedbydev/Systemhook/refs/heads/main/cursor.lua"))()
+loadstring(HttpGet("https://raw.githubusercontent.com/devvedbydev/Systemhook/refs/heads/main/skeletonesp.lua"))()
+loadstring(HttpGet("https://raw.githubusercontent.com/devvedbydev/Systemhook/refs/heads/main/overlay.lua"))()
