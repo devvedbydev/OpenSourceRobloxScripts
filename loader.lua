2ca404bfdbb5a8d21412929fd3df33c3ab4f74a8
Dev <79853619+devvedbydev@users.noreply.github.com>
2024-10-09 23:09:27 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,4 @@
 loadstring(HttpGet("https://raw.githubusercontent.com/hitbyharibo/byfron/main/aimlock.lua"))()
-loadstring(HttpGet("https://raw.githubusercontent.com/hitbyharibo/byfron/main/flyto"))()
+loadstring(HttpGet("https://raw.githubusercontent.com/hitbyharibo/byfron/main/cursor.lua"))()
+loadstring(HttpGet("https://raw.githubusercontent.com/hitbyharibo/byfron/main/skeletonesp.lua"))()
+loadstring(HttpGet("https://raw.githubusercontent.com/hitbyharibo/byfron/main/overlay.lua"))()
