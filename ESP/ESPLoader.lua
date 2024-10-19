a857759d628920aab2c3963938a9ed3da19c4122
Dev <79853619+devvedbydev@users.noreply.github.com>
2024-10-19 14:52:11 +0100

    Create ESPLoader.lua

---
+++ ESP/ESPLoader.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/devvedbydev/Systemhook/refs/heads/main/ESP/Health.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/devvedbydev/Systemhook/refs/heads/main/ESP/Skeleton.lua'))()
